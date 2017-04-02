a77e83755d6ac4d00aabc07189a5c7ab8e545ad7
Edson Alves <edsomjr@gmail.com>
2017-04-03 01:44:06 +0300

    Lua compiling with CMake.

--- linux/Linux.cmake
+++ linux/Linux.cmake
@@ -1,4 +1,4 @@
 cmake_minimum_required(VERSION 2.6)
 
 set(SYSTEM linux)
-set(SYSTEM_LIBRARIES libSDL2.so libSDL2_image.so)
+set(SYSTEM_LIBRARIES libSDL2.so libSDL2_image.so libSDL2_ttf.so libSDL2_mixer.so libSDL2_gfx.so tmxparser liblua.so -ldl)
